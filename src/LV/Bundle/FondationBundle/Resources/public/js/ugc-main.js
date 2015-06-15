797d3e3eef1ca69e12733e4d14d3d9718e15c959
Vincen Wen <vincen.wen@samesamechina.com>
2015-06-15 09:52:46 +0800

    no message

--- src/LV/Bundle/FondationBundle/Resources/public/js/ugc-main.js
+++ src/LV/Bundle/FondationBundle/Resources/public/js/ugc-main.js
@@ -98,6 +98,7 @@ var _doing = {
 			        		}
 			        	}
 			        	//console.log(_doing.curChoseGlassDreamNum);
+			        	alert(_doing.curChoseGlassDreamNum%5)
 			        	$("#cur-glass").attr("src","/images/ugc/glass-bg-"+_doing.curChoseGlassDreamNum%5+".jpg")
 				    }
 

c74699cf5fb2b0624d640c7ed4b0021332cd6e19
Vincen Wen <vincen.wen@samesamechina.com>
2015-06-15 10:27:06 +0800

    修复切换问题

--- src/LV/Bundle/FondationBundle/Resources/public/js/ugc-main.js
+++ src/LV/Bundle/FondationBundle/Resources/public/js/ugc-main.js
@@ -73,7 +73,7 @@ var _doing = {
 							islikecon = "已支持";
 						}
 						 
-						return '<div class="swiper-slide"><img src="" sourcesrc="/images/ugc/glass-bg-'+k%5+'.jpg" width="100%" class="cur-glass" /><div class="hotArea"><div class="supportIcon '+islikeVal+'" data-dream-id="'+v.dream_id+'"><img src="/images/ugc/like'+islikeVal+'.png" width="100%" /><i>'+islikecon+'</i></div><div class="shareIcon"><img src="/images/ugc/shareIcon.png" width="100%" /><i>分享</i></div></div><div class="dreamMode"><div class="selectedModel_con"><div class="selectedNum">'+v.dream_num+'</div><div class="selectedCon">'+v.content+'</div><div class="selectedName">'+v.nickname+'</div></div><img src="/images/ugc/list-model-'+k%5+'.png" width="100%" /></div></div>'
+						return '<div class="swiper-slide"><img src="/images/ugc/glass-bg-'+k%5+'.jpg" width="100%" class="cur-glass" /><div class="hotArea"><div class="supportIcon '+islikeVal+'" data-dream-id="'+v.dream_id+'"><img src="/images/ugc/like'+islikeVal+'.png" width="100%" /><i>'+islikecon+'</i></div><div class="shareIcon"><img src="/images/ugc/shareIcon.png" width="100%" /><i>分享</i></div></div><div class="dreamMode"><div class="selectedModel_con"><div class="selectedNum">'+v.dream_num+'</div><div class="selectedCon">'+v.content+'</div><div class="selectedName">'+v.nickname+'</div></div><img src="/images/ugc/list-model-'+k%5+'.png" width="100%" /></div></div>'
 					
 
 				})

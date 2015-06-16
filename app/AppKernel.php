0b910b3d53e5a72c2ab0db210d97ee3b49cac77b
Albert Shen <albertshen1206@gmail.com>
2015-06-16 15:13:39 +0800

    update

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new AppBundle\AppBundle(),
             new LV\Bundle\FondationBundle\LVFondationBundle(),
             new Same\Bundle\WechatBundle\SameWechatBundle(),
             new Same\Bundle\SessionBundle\SameSessionBundle(),

873c38f93026e79ab3268b726fd7671de22af6cc
Albert Shen <albertshen1206@gmail.com>
2015-06-16 11:51:20 +0800

    clean the code

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
             new LV\Bundle\FondationBundle\LVFondationBundle(),
             new Same\Bundle\WechatBundle\SameWechatBundle(),
             new Same\Bundle\SessionBundle\SameSessionBundle(),

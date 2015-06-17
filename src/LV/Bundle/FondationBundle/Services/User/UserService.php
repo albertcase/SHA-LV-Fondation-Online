a36f067393ed1b33df3da7debd095ef7b2bc157a
Albert Shen <albertshen1206@gmail.com>
2015-06-17 13:50:33 +0800

    update

--- src/LV/Bundle/FondationBundle/Services/User/UserService.php
+++ src/LV/Bundle/FondationBundle/Services/User/UserService.php
@@ -36,9 +36,8 @@ class UserService
     */
     public function userIsLogin()
     {
-        // $session = $this->requestStack->getCurrentRequest()->getSession();
-        // $uid = $session->get('user');
-        $uid = isset($_COOKIE['user']) ? $_COOKIE['user'] : 0;
+        $session = $this->requestStack->getCurrentRequest()->getSession();
+        $uid = $session->get('user');
         if($uid) {
             return $uid;
         }
@@ -80,9 +79,8 @@ class UserService
     {
         $user = $this->findUserByOpenId($openid);
         $user = $user ? $user : $this->userRegister($openid);
-        setcookie("user", $user->getId(), time()+3600*24);
-        // $session = $this->requestStack->getCurrentRequest()->getSession();
-        // $session->set('user', $user->getId());
+        $session = $this->requestStack->getCurrentRequest()->getSession();
+        $session->set('user', $user->getId());
         return $user;
         // $this->requestStack->getCurrentRequest()->cookies;
         // $cookie = new Cookie('openid', $openid, time() + 3600 * 24 * 7);

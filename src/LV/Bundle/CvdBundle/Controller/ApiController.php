bbd1e1c69daf5bbb237994a3b332aec566a4fe15
Albert Shen <albertshen1206@gmail.com>
2015-08-06 15:46:20 +0800

    update

---
+++ src/LV/Bundle/CvdBundle/Controller/ApiController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace LV\Bundle\CvdBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class ApiController extends Controller
+{
+    public function createAction()
+    {
+        
+        // $response = new JsonResponse();
+        // $response->setData(array('status' => $status));
+        // return $response;
+    }
+}

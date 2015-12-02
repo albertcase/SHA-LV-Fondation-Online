0523b60f88de63e700f0289f8267d02841e53d0b
Demon Zhang <demon.zhang@samesamechina.com>
2015-12-02 19:29:20 +0800

    no message

--- src/LV/Bundle/NdsBundle/Controller/DefaultController.php
+++ src/LV/Bundle/NdsBundle/Controller/DefaultController.php
@@ -15,16 +15,16 @@ class DefaultController extends Controller
 
     	$user = $this->get('lv.user.service');
     	$wechat = $this->get('same.wechat');
-    	// if(!$user->userLoad()) {
+    	if(!$user->userLoad()) {
 
-	    //     $url = $this->getRequest()->getRequestUri();
+	        $url = $this->getRequest()->getRequestUri();
 
-	    //     $isWechatLogin = $wechat->isLogin($url);
-	    //     if($isWechatLogin instanceof RedirectResponse)
-	    //        return $isWechatLogin;
+	        $isWechatLogin = $wechat->isLogin($url);
+	        if($isWechatLogin instanceof RedirectResponse)
+	           return $isWechatLogin;
 
-	    //    $user->userLogin($isWechatLogin);
-	    //}
+	       $user->userLogin($isWechatLogin);
+	    }
 	    $subscribe = 1;
         return $this->render('LVNdsBundle:Default:index.html.twig', array('subscribe' => $subscribe));
 
@@ -44,7 +44,7 @@ class DefaultController extends Controller
         if(!$log){
             $ndsinfo = new Ndsinfo();
             $ndsinfo->setUser($user);
-            $ndsinfo->setName($type);     
+            $ndsinfo->setName($name);     
             $ndsinfo->setMobile($mobile);
             $ndsinfo->setSex($sex);
             $ndsinfo->setCity($city);

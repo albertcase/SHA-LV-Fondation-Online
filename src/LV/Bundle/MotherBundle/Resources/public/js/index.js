0a067e3ad570466c3b2645bd3ddf44ccb19cdb59
Vincen Wen <vincen.wen@samesamechina.com>
2016-04-26 17:18:48 +0800

    no message

--- src/LV/Bundle/MotherBundle/Resources/public/js/index.js
+++ src/LV/Bundle/MotherBundle/Resources/public/js/index.js
@@ -67,9 +67,8 @@ var myslider, shakeId, shakeArr = ["1", "2", "3", "fulllist", "4", "5", "6", "fu
     
     
     function shareFun(){
-        $("#sharePup").css({"height": "100%"}).animate({"opacity": 1}, function(){
-            $(".messageTextarea").focus();
-        });
+        $("#sharePup").css({"height": "100%"}).css({"opacity": 1});
+        $(".messageTextarea").focus();
         _hmt.push(['_trackEvent', 'btn', 'click', "点击事件：share"]);
     }
 

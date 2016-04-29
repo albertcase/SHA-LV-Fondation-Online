720760aa1bb30ee98f5c73323dfbd1d75098ffab
Vincen Wen <vincen.wen@samesamechina.com>
2016-04-29 17:44:52 +0800

    i

--- src/LV/Bundle/MotherBundle/Resources/public/js/public.js
+++ src/LV/Bundle/MotherBundle/Resources/public/js/public.js
@@ -100,7 +100,7 @@ function shareFunSetDefault(){
             title: _unescape('路易威登•母亲节温情献礼'),
             desc: _unescape('无尽感恩，在这个母亲节化为永恒礼赞。'),
             link: window.location.host,
-            imgUrl: 'http://' + window.location.host + '/images/motherdayImg/share.jpg', 
+            imgUrl: 'http://7vzs67.com1.z0.glb.clouddn.com/3837a392-95a4-49c3-8231-71cf9b44119e?imageView2/1/w/200/h/200/format/jpg/q80/interlace/1', 
             trigger: function (res) {
                 //  alert('用户点击发送给朋友');
             },
@@ -122,7 +122,7 @@ function shareFunSetDefault(){
         wx.onMenuShareTimeline({
             title: _unescape('路易威登•母亲节温情献礼'), 
             link: window.location.host,
-            imgUrl: 'http://' + window.location.host + '/images/motherdayImg/share.jpg', 
+            imgUrl: 'http://7vzs67.com1.z0.glb.clouddn.com/3837a392-95a4-49c3-8231-71cf9b44119e?imageView2/1/w/200/h/200/format/jpg/q80/interlace/1', 
             trigger: function (res) {
                 //   alert('用户点击分享到朋友圈');
             },
